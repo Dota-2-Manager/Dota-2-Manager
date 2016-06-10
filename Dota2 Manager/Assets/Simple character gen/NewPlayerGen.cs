1665ddd0b5f96be2761a037fcae996aef8abf0cd
Kristiyan Lazarov <kristyan.lazarov@gmail.com>
2016-06-10 18:25:12 +0200

    added some starter classes made by blaze

---
+++ Dota2 Manager/Assets/Simple character gen/NewPlayerGen.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class NewPlayerGen : MonoBehaviour {
+
+    public List<DotaPlayer> players;
+    public DotaPlayer a;
+    // Use this for initialization
+    void Start () {
+        players = new List<DotaPlayer>();
+        MakeNewPlayer();
+        MakeNewPlayer();
+        MakeNewPlayer();
+
+        foreach (DotaPlayer player in players)
+        {
+            Debug.Log(player.total);
+        }
+       
+
+    }
+	
+	// Update is called once per frame
+	void MakeNewPlayer ()
+    {
+        a = new DotaPlayer();
+        a.name = "fred";
+        a.farming = Random.Range(10,25);
+        a.lastHitting = Random.Range(10, 25);
+        a.denying = Random.Range(10, 25);
+        a.Pushing = Random.Range(10, 25);
+        a.decisionMaking = Random.Range(10, 25);
+        a.rage = Random.Range(10, 25);
+        a.stress = 0;
+        a.CalculateTotal();
+        players.Add(a);
+        
+	}
+
+}
---
+++ Dota2 Manager/Assets/Simple character gen/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DotaPlayer : MonoBehaviour {
+
+
+    public string name;
+
+    //postive
+    public double farming;
+    public double lastHitting;
+    public double denying;
+    public double Pushing;
+    public double decisionMaking;
+    public double rage;
+
+    //negative
+    public double stress;
+
+    //total
+    public double total;
+
+    void Start () {
+        CalculateTotal();
+    }
+
+    public void CalculateTotal()
+    {
+        total = farming * 0.145 + lastHitting * 0.145 + denying * 0.145 + Pushing * 0.145 + decisionMaking * 0.145 + rage * 0.145 - stress * 0.145;
+    }
+	
+
+}
