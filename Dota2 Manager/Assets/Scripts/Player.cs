0763d2c2757662acf342e2b9ac3db1813be7e81b
Lopuhan <zug209@gmail.com>
2016-06-14 12:38:10 +0300

    Player.cs housekeeping

--- Dota2 Manager/Assets/Scripts/Player.cs
+++ Dota2 Manager/Assets/Scripts/Player.cs
@@ -10,26 +10,26 @@ public class DotaPlayer{
     private Position CurrentRole;
 
     //newStats v0.1
-    private double pushing;
-    private double farming;
-    private double fighting;
-    private double warding;
-    private double Positioning;
-    private double mapAwareness;
-    private double decisionMakeing;
-    private double roaming;
-    private double laneControl;
-    private double riskTakeing;
-    private double flair;
-    private double Consistency;
-    private double teamWork;
-    private double Leadership;
+    private double pushing { get; set; }
+    private double farming { get; set; }
+    private double fighting { get; set; }
+    private double warding { get; set; }
+    private double Positioning { get; set; }
+    private double mapAwareness { get; set; }
+    private double decisionMakeing { get; set; }
+    private double roaming { get; set; }
+    private double laneControl { get; set; }
+    private double riskTakeing { get; set; }
+    private double flair { get; set; }
+    private double Consistency { get; set; }
+    private double teamWork { get; set; }
+    private double Leadership { get; set; }
 
     //positions
-    private double Carry;
-    private double Mid;
-    private double Offlane;
-    private double Support;
+    private double Carry { get; set; }
+    private double Mid { get; set; }
+    private double Offlane { get; set; }
+    private double Support { get; set; }
 
     //total
     public double total;
@@ -61,176 +61,25 @@ public class DotaPlayer{
 
     public void setAll(double a, double b, double c, double d, double e, double f, double g, double h, double i, double j, double k, double l, double m,double n,double o, double p, double q, double r)
     {
-        setCarry(a);
-        setConsistency(b);
-        setdecisionMakeing(c);
-        setfarming(d);
-        setfighting(e);
-        setflair(f);
-        setlaneControl(g);
-        setLeadership(h);
-        setmapAwareness(i);
-        setMid(j);
-        setOfflane(k);
-        setPositioning(l);
-        setpushing(m);
-        setrisktakeing(n);
-        setRoaming(o);
-        setSupport(p);
-        setteamwork(q);
-        setWarding(r);
-    }
-
-
-    //setters
-
-    public void setpushing(double num)
-    {
-        pushing = num;
-    }
-    public void setfarming(double num)
-    {
-        farming = num;
-    }
-    public void setfighting(double num)
-    {
-        fighting = num;
-    }
-    public void setWarding(double num)
-    {
-        warding = num;
-    }
-    public void setPositioning(double num)
-    {
-        Positioning = num;
-    }
-    public void setmapAwareness(double num)
-    {
-        mapAwareness = num;
-    }
-    public void setRoaming(double num)
-    {
-        roaming = num;
-    }
-    public void setdecisionMakeing(double num)
-    {
-        decisionMakeing = num;
-    }
-    public void setlaneControl(double num)
-    {
-        laneControl = num;
-    }
-    public void setrisktakeing(double num)
-    {
-        riskTakeing = num;
-    }
-    public void setflair(double num)
-    {
-        flair = num;
-    }
-    public void setConsistency(double num)
-    {
-        Consistency = num;
-    }
-    public void setteamwork(double num)
-    {
-        teamWork = num;
-    }
-    public void setLeadership(double num)
-    {
-        Leadership = num;
-    }
-    public void setCarry(double num)
-    {
-        Carry = num;
-    }
-    public void setMid(double num)
-    {
-        Mid = num;
-    }
-    public void setOfflane(double num)
-    {
-        Offlane = num;
-    }
-    public void setSupport(double num)
-    {
-        Support = num;
-    }
-
-    //getters
-
-    public double getpushing()
-    {
-        return pushing;
-    }
-    public double getfarming()
-    {
-        return farming;
-    }
-    public double getfighting()
-    {
-        return fighting;
-    }
-    public double getWarding()
-    {
-        return warding;
-    }
-    public double getPositioning()
-    {
-        return Positioning;
-    }
-    public double getmapAwareness()
-    {
-        return mapAwareness;
-    }
-    public double getRoaming()
-    {
-        return roaming;
-    }
-    public double getdecisionMakeing()
-    {
-        return decisionMakeing;
-    }
-    public double getlaneControl()
-    {
-        return laneControl;
-    }
-    public double getrisktakeing()
-    {
-        return riskTakeing;
-    }
-    public double getflair()
-    {
-        return flair;
-    }
-    public double getConsistency()
-    {
-        return Consistency;
-    }
-    public double getteamwork()
-    {
-        return teamWork;
-    }
-    public double getLeadership()
-    {
-        return Leadership;
-    }
-    public double getCarry()
-    {
-        return Carry;
-    }
-    public double getMid()
-    {
-        return Mid;
-    }
-    public double getOfflane()
-    {
-        return Offlane;
-    }
-    public double getSupport()
-    {
-        return Support;
-    }
+        Carry = a;
+        Consistency = b;
+        decisionMakeing = c;
+        farming = d;
+        fighting = e;
+        flair = f;
+        laneControl = g;
+        Leadership = h;
+        mapAwareness = i;
+        Mid = j;
+        Offlane = k;
+        Positioning = l;
+        pushing = m;
+        riskTakeing = n;
+        roaming = 0;
+        Support = p;
+        teamWork = q;
+        warding = r;
+    }    
 }
 
 
