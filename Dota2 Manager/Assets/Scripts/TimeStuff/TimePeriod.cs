e834d1e5c7342b4b9a1d6e688d42374a0b873654
Lopuhan <zug209@gmail.com>
2016-06-17 11:18:34 +0300

    Time branch START

---
+++ Dota2 Manager/Assets/Scripts/TimeStuff/TimePeriod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.TimeStuff
+{
+    class TimePeriod
+    {
+        DateTime _start;
+        public DateTime Start
+        { get { return _start; } }
+
+        DateTime _end;
+        public DateTime End
+        { get { return _end; } }
+    }
+}
